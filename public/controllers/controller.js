fcb98f2bbb49400985a7bac06dc68295df6b0a1a
rbburkey <robinsonbburkey@gmail.com>
2016-07-08 10:25:06 -0700

    changed dummy data from controller to local server

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -7,24 +7,11 @@ var myApp = angular.module('myApp', []);
 myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http) {
     console.log("Hello World from controller");
 
-    restaurant1 = {
-      name: "Yamo",
-      image: "None submitted",
-      review: "Cheap restaurant, but gave me a stomach ache"
-    }
+    $http.get('/restaurantReviews').success(function(response) {
+      console.log('I got the data I requested');
+      $scope.restaurantReviews = response;
+    });
 
-    restaurant2 = {
-      name: "Curry up now",
-      image: "None submitted",
-      review: "Great indian food, but a tad expensive"
-    }
 
-    restaurant2 = {
-      name: "taqueria",
-      image: "None submitted",
-      review: "Cheap and great"
-    }
 
-    var restaurantReviews = [restaurant1, restaurant2, restaurant3];
-    $scope.restaurantReviews = restaurantReviews;
 }]);
--- server.js
+++ server.js
@@ -3,5 +3,29 @@ var app = express();
 
 app.use(express.static(__dirname + '/public'));
 
+app.get('/restaurantReviews', function(req, res) {
+  console.log('I recieved a get request');
+  restaurant1 = {
+    name: "Yamo",
+    image: "None submitted",
+    review: "Cheap restaurant, but gave me a stomach ache"
+  }
+
+  restaurant2 = {
+    name: "Curry up now",
+    image: "None submitted",
+    review: "Great indian food, but a tad expensive"
+  }
+
+  restaurant3 = {
+    name: "taqueria",
+    image: "None submitted",
+    review: "Cheap and great"
+  }
+
+  var restaurantReviews = [restaurant1, restaurant2, restaurant3];
+  res.json(restaurantReviews);
+});
+
 app.listen(3000);
 console.log("server running on port 3000");
