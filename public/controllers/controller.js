2a93a0ce80cc352ddd3fdadcd3b856ee183e51c0
rbburkey <robinsonbburkey@gmail.com>
2016-07-12 13:18:15 -0400

    added parcer and added editing and update features

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -1,13 +1,40 @@
 var myApp = angular.module('myApp', []);
 myApp.controller('AppCtrl', ['$scope', '$http', function ($scope, $http) {
-    console.log("Hello World from controller");
+  console.log("Hello World from controller");
 
-// function AppCtrl($scope, $http) {
-//   console.log("Hello World from Controller");
+var refresh = function(){ //this refresh function performs like an ajax function it autopopulates the page with the new data
+  $http.get('/restaurantReviews').success(function (response) {
+    console.log('I got the data I requested');
+    $scope.restaurantReviews = response;
+    $scope.restaurant = "";
+  });
+};
+refresh();
 
-    $http.get('/restaurantReviews').success(function (response) {
-      console.log('I got the data I requested');
-      $scope.restaurantReviews = response;
+  //addRestaurant refers to the add restaurant button in the index html (the button)
+  $scope.addRestaurant = function() {
+    console.log($scope.restaurant);
+    // restaurantReviews is our route and $scope.reataurant is the data we are sending to the server
+    // the server needs to now listen for this request     app.post
+    $http.post('/restaurantReviews', $scope.restaurant).success(function(response) {
+      console.log(response);
+      refresh();
     });
+  };
+
+  $scope.remove = function(id) {
+    console.log(id);
+    $http.delete('/restaurantReviews/' + id).success(function(response) {
+      refresh();
+    });
+  };
+
+  $scope.edit = function(id) {
+    console.log(id);
+    $http.get('/restaurantReviews/' + id).success(function(response) {
+      $scope.restaurant = response;
+    });
+
+  };
 
 }]);
--- server.js
+++ server.js
@@ -2,15 +2,42 @@ var express = require('express');
 var app = express();
 var mongojs = require('mongojs');
 var db = mongojs('restaurantReviews', ['restaurantReviews']);
+var bodyParser = require('body-parser');
 
 app.use(express.static(__dirname + '/public'));
+// this enables the server to parse the body of the input in a digestable form on line 33
+app.use(bodyParser.json());
 
 app.get('/restaurantReviews', function(req, res) {
   console.log('I recieved a get request');
-
   db.restaurantReviews.find(function(err, docs) {
     console.log('docs');
     res.json(docs);
+  });
+});
+
+// app.post listens to the post request from the controller, server needs to know how to parse the body of the input....
+// this code inputs data into the database as well as sends the data back to the controller
+app.post('/restaurantReviews', function(req, res){
+  console.log(req.body);
+  db.restaurantReviews.insert(req.body, function(err,doc) {
+    res.json(doc);
+  });
+});
+
+app.delete('/restaurantReviews/:id', function(req, res) {
+  var id = req.params.id;
+  console.log(id);
+  db.restaurantReviews.remove({_id: mongojs.ObjectId(id)}, function(err, doc) {
+    res.json(doc);
+  });
+});
+
+app.get('/restaurantReviews/:id', function(req, res) {
+  var id = req.params.id;
+  console.log(id);
+  db.restaurantReviews.findOne({_id: mongojs.ObjectId(id)}, function(err, doc) {
+    res.json(doc);
   });
 });
 
